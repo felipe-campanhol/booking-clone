dd27ba561f885a4b964018044dbdb0c8500456c9
Felipe Campanhol Manoel de Jesus <fcampanhol@hotmail.com>
2024-01-14 22:31:00 -0300

    Commit inicial do projeto booking.com

---
+++ data/trending.ts
@@ -0,0 +1,37 @@
+export const trending_data = [
+    {
+        id: 1,
+        src: "https://res.klook.com/image/upload/Mobile/City/shqqq2k8g4wpfsa39mod.jpg",
+        title: "Dubai",
+        location: "United Arab Emirates",
+        description: "15 Deals",
+    },
+    {
+        id: 2,
+        src: "http://www.travelweekly.com/uploadedImages/All_TW_Art/Shutterstock_Art/2017/Seoul.jpg?n=9242&width=1540&height=866&mode=crop&Anchor=MiddleCenter",
+        title: "South Korea",
+        location: "Asia",
+        description: "32 Deals",
+    },
+    {
+        id: 3,
+        src: "https://c4.wallpaperflare.com/wallpaper/882/291/768/cityscape-city-london-united-kingdom-wallpaper-preview.jpg",
+        title: "London",
+        location: "United Kingdom",
+        description: "45 Deals",
+    },
+    {
+        id: 4,
+        src: "https://www.pixelstalk.net/wp-content/uploads/images1/Best-singapore-wallpapers.jpg",
+        title: "Singapure",
+        location: "Asia",
+        description: "88 Deals",
+    },
+    {
+        id: 5,
+        src: "https://worldstrides.com/wp-content/uploads/2015/07/iStock_000040849990_Large.jpg",
+        title: "New York",
+        location: "United States of America",
+        description: "92 Deals",
+    },
+]
